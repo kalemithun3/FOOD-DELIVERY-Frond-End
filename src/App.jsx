15aa5693dab1cdabc8be0be6e04cef70446fbc0a
Megha Kale <mithunkalem2@gmail.com>
2024-07-24 19:22:00 +0530

    navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from "./components/Navbar/Navbar";
-
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/order" element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import { useState } from "react";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+    </div>
+  );
+};
 
 export default Home;
